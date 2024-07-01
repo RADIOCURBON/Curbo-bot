900d2e06163ed834d98ce96ff559a3ed291911d0
RADIOCURBON <vova.bima.vika@gmail.com>
2024-07-01 21:00:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import telebot
 from telebot import types
+import schedule
+import time
+from threading import Thread
 
-bot = telebot.TeleBot('7051824056:AAEtHdmndutwQYFFpi8oPz8AToupXF-Xxlo')
+bot = telebot.TeleBot('')
+
+user_messages = {}  # Словарь для хранения последних сообщений пользователей
 
 def start_markup():
     markup = types.InlineKeyboardMarkup(row_width=True)
@@ -12,24 +17,34 @@ def start_markup():
 
 def thank_you_markup():
     markup = types.InlineKeyboardMarkup(row_width=True)
-    mini_web_button = types.InlineKeyboardButton(text='Открыть приложение !', url='t.me/Curbo_bot/app')
+    mini_web_button = types.InlineKeyboardButton(text='Открыть приложение !', url='')
     markup.add(mini_web_button)
     return markup
-
 
 @bot.message_handler(commands=['start'])
 def start(message):
     chat_id = message.chat.id
     first_name = message.chat.first_name
-    bot.send_message(chat_id, f"Привет {first_name}!\n"
-                     "Чтобы пользоваться ботом, подпишитесь на данный канал", reply_markup=start_markup())
+    if chat_id in user_messages:
+        try:
+            bot.delete_message(chat_id, user_messages[chat_id])
+        except:
+            pass
+    sent_message = bot.send_message(chat_id, f"Привет {first_name}!\nЧтобы пользоваться ботом, подпишитесь на данный канал", reply_markup=start_markup())
+    user_messages[chat_id] = sent_message.message_id
 
 def check(call):
     try:
         status = ['creator', 'administrator', 'member']
         user_status = bot.get_chat_member(chat_id='-1002249640348', user_id=call.message.chat.id).status
         if user_status in status:
-            bot.send_message(call.message.chat.id, "Спасибо, что подписались на канал!", reply_markup=thank_you_markup())
+            if call.message.chat.id in user_messages:
+                try:
+                    bot.delete_message(call.message.chat.id, user_messages[call.message.chat.id])
+                except:
+                    pass
+            sent_message = bot.send_message(call.message.chat.id, "Спасибо, что подписались на канал!", reply_markup=thank_you_markup())
+            user_messages[call.message.chat.id] = sent_message.message_id
         else:
             bot.send_message(call.message.chat.id, 'Подпишитесь на канал!', reply_markup=start_markup())
     except telebot.apihelper.ApiTelegramException as e:
@@ -39,5 +54,30 @@ def check(call):
 def callback(call):
     if call.data == 'check':
         check(call)
+
+def periodic_check():
+    for chat_id in user_messages.keys():
+        try:
+            status = ['creator', 'administrator', 'member']
+            user_status = bot.get_chat_member(chat_id='-1002249640348', user_id=chat_id).status
+            if user_status not in status:
+                if chat_id in user_messages:
+                    try:
+                        bot.delete_message(chat_id, user_messages[chat_id])
+                    except:
+                        pass
+                sent_message = bot.send_message(chat_id, 'Подпишитесь на канал!', reply_markup=start_markup())
+                user_messages[chat_id] = sent_message.message_id
+        except telebot.apihelper.ApiTelegramException as e:
+            bot.send_message(chat_id, f'Произошла ошибка: {e.description}')
+
+def schedule_checker():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+schedule.every(10).minutes.do(periodic_check)  # Проверка каждые 10 минут
+
+Thread(target=schedule_checker).start()
 
 bot.polling(none_stop=True)
